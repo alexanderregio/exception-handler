475b521d3cecf3e72581399d54e0b90b7b65540f
Alexander Régio da Fonseca <alexanderregio@hotmail.com>
2022-03-05 11:02:08 -0300

    Implementação das classes abstratas CoreError e CoreException

---
+++ ExceptionHandler.Exceptions/CoreError.cs
@@ -0,0 +1,14 @@
+﻿namespace ExceptionHandler.Exceptions;
+
+public abstract class CoreError
+{
+    public readonly string Key;
+
+    public readonly string Message;
+
+    protected CoreError(string key, string message)
+    {
+        Key = key;
+        Message = message;
+    }
+}
